605e170156be5643ac493fa1f3a9c8fc11c5b1ab
alexarmatas <alexarmatas97@gmail.com>
2020-02-25 09:41:49 -0500

    adding slideshow to index

--- main.js
+++ main.js
@@ -1,38 +1,73 @@
+const nav = document.querySelector("nav");
 const navLinks = document.querySelector(".navLinks");
 const navchildNodes = navLinks.childNodes;
 const bubble = document.querySelector(".bubble");
 
-navchildNodes.forEach(node => {
-  if (node.nodeType == 1) {
-    if (node.tagName == "LI") {
-      node.addEventListener("mouseover", () => {
-        const coords = node.getBoundingClientRect();
-        const directions = {
-          height: coords.height,
-          width: coords.width,
-          top: coords.top,
-          left: coords.left
-        };
-        bubble.style.setProperty("left", `${directions.left}px`);
-        bubble.style.setProperty("top", `${directions.top}px`);
-        bubble.style.setProperty("width", `${directions.width}px`);
-        bubble.style.setProperty("height", `${directions.height}px`);
-      });
+//Navbar Function
+function setBubble() {
+  const coords = this.getBoundingClientRect();
+  const directions = {
+    height: coords.height,
+    width: coords.width,
+    top: coords.top,
+    left: coords.left
+  };
+  bubble.style.opacity = 1;
+  bubble.style.setProperty("left", `${directions.left}px`);
+  bubble.style.setProperty("top", `${directions.top}px`);
+  bubble.style.setProperty("width", `${directions.width}px`);
+  bubble.style.setProperty("height", `${directions.height}px`);
+}
+
+nav.addEventListener("mouseleave", () => {
+  navchildNodes.forEach(node => {
+    if (node.nodeType == 1) {
+      node.removeEventListener("mouseover", setBubble);
     }
-    /*
-    coords = node.getBoundingClientRect();
-    const directions = {
-      height: coords.height,
-      width: coords.width,
-      top: coords.top,
-      left: coords.left
-    };
-    node.addEventListener("mouseover", () => {
-      bubble.style.setProperty("left", `${directions.left}px`);
-      bubble.style.setProperty("top", `${directions.top}px`);
-      bubble.style.setProperty("width", `${directions.width}px`);
-      bubble.style.setProperty("height", `${directions.height}px`);
-    });
-    */
-  }
+  });
+  bubble.style.opacity = 0;
 });
+
+nav.addEventListener("mouseover", () => {
+  navchildNodes.forEach(node => {
+    if (node.nodeType == 1) {
+      if (node.tagName == "LI") {
+        node.addEventListener("mouseover", setBubble);
+      }
+    }
+  });
+});
+
+//Slide Functionality
+var slideIndex = 1;
+
+//next / previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+//Thumbnail image control
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    console.log(slideIndex);
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
